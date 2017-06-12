5e603604c9502a0092567d41a397d593810585e8
chanmi Park <chanmi.park@cdnetworks.co.kr>
2017-06-12 13:15:31 +0900

    test2

--- snippets/urls.py
+++ snippets/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(), name='snippet-detail'),
     url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(), name='snippet-highlight'),
 
-
     url(r'^users/$', views.UserList.as_view(), name='user-list'),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
 ]
